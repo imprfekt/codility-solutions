e4296b23c171517ef18eeb561b05e953122ff664
Mališa Stanojević <malisa.stanojevic@fishingbooker.com>
2018-07-28 17:42:45 +0200

    Sorting/Distinct

---
+++ Sorting/Distinct.php
@@ -0,0 +1,22 @@
+// you can write to stdout for debugging purposes, e.g.
+// print "this is a debug message\n";
+
+function solution($A) {
+    $distinct = 0;
+    
+    sort($A);
+
+    for($i = 0; $i < count($A) - 1; $i++){
+        if($A[$i] !== $A[$i+1]){
+            $distinct++;
+        }
+    }
+    
+    //we counted distinct pairs
+    //and need to add +1 for distinct values
+    if(count($A) > 0){
+        $distinct++;
+    }
+    
+    return $distinct;
+}
