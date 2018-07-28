0d8e238a1930437aa5ab593f91497337df716a61
Mališa Stanojević <malisa.stanojevic@fishingbooker.com>
2018-07-29 01:47:40 +0200

    Sorting/NumberOfDiscIntersections

---
+++ Sorting/NumberOfDiscIntersections.php
@@ -0,0 +1,29 @@
+<?php 
+
+function solution($A) {
+    $min = [];
+    $max = [];
+    $ranges= [];
+    $result = 0;
+    foreach($A as $i => $num){
+        $ranges[$i] = ["min" => $i - $num, "max" => $i + $num];
+    }
+    
+    sort($ranges);
+    //var_dump($ranges);
+    
+    for($i = 0; $i < count($ranges) - 1; $i++){
+        for($j = $i + 1; $j < count($ranges); $j++){
+           if($ranges[$j]["min"] <= $ranges[$i]["max"]){
+                $result++;
+            }else{
+                break;
+            }
+        }
+    }
+    //asort($min);
+    //var_dump($min);
+    //echo implode(", ", $min);
+    //var_dump($max);
+    return $result;
+}
